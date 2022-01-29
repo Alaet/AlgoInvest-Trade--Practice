bcdf26f453048a0cbb8e2948968496edb534b8bf
Alaet <72195619+Alaet@users.noreply.github.com>
2022-01-29 12:38:17 +0100

    Update optimized.py
    renaming, refract of main code

--- optimized.py
+++ optimized.py
@@ -1,40 +1,38 @@
 import csv
+from data.actions import actions as small_list_actions
 
 
 def read_files(files_names):
-    actions = []
+    actions_list = []
     for file_name in files_names:
         file = open(file_name)
         csvreader_file = csv.reader(file)
-        for action in csvreader_file:
-            if action[1] != "price":
-                action[1] = float(action[1])
-                action[2] = float(action[2])
-                if action[1] > 0 and action[2] > 0:
-                    actions.append(action)
-    return actions
+        for action_row in csvreader_file:
+            if action_row[1] != "price":
+                action_row[1] = float(action_row[1])
+                action_row[2] = float(action_row[2])
+                if action_row[1] > 0 and action_row[2] > 0:
+                    actions_list.append(action_row)
+    return actions_list
 
 
-actions = read_files(["data/dataset1_Python.csv", "data/dataset2_Python.csv"])
-
-
-def get_wallet_benefit(actions):
+def get_wallet_benefit(actions_to_evaluate):
     benefit_best_wallet = 0
-    for act in actions:
+    for act in actions_to_evaluate:
         benefit_best_wallet += (act[2] / 100) * act[1]
     return benefit_best_wallet
 
 
-def get_wallet_invest_cost(actions):
-    invest = 0
-    for act in actions:
-        invest += act[1]
-    return invest
+def get_wallet_invest_cost(actions_to_evaluate):
+    wallet_investment = 0
+    for act in actions_to_evaluate:
+        wallet_investment += act[1]
+    return wallet_investment
 
 
 def get_best_wallet(actions, capital):
     i = 0
-    actions.sort(key=lambda x:x[2], reverse=True)
+    actions.sort(key=lambda x: x[2], reverse=True)
     best_wallet = []
 
     while i <= capital:
@@ -46,7 +44,7 @@ def get_best_wallet(actions, capital):
             if current_action[1] <= allocated_capital:
                 current_wallet.append(current_action)
                 allocated_capital -= current_action[1]
-                current_wallet_benefit += (current_action[2]/100)*current_action[1]
+                current_wallet_benefit += (current_action[2] / 100) * current_action[1]
 
         benefit = get_wallet_benefit(best_wallet)
         if current_wallet_benefit > benefit and len(current_wallet) > 0:
@@ -56,15 +54,18 @@ def get_best_wallet(actions, capital):
     return best_wallet
 
 
-wallet = get_best_wallet(actions, 500)
+csv_actions = read_files(["data/dataset1_Python.csv", "data/dataset2_Python.csv"])
+list_actions = small_list_actions
+wallet = get_best_wallet(csv_actions, 500)
 benefit_wallet = get_wallet_benefit(wallet)
 invest = get_wallet_invest_cost(wallet)
 
 print("Somme investie : %s€." % round(invest, 2))
 print("Bénéfices attendus /2ans : %s€." % round(benefit_wallet, 2))
-print("Rentabilité : %s%%." % (round((benefit_wallet/invest)*100,2)))
+print("Rentabilité : %s%%." % (round((benefit_wallet / invest) * 100, 2)))
 print("Pour %s actions acquises." % len(wallet))
 if input("\n************     1 = Afficher liste des actions     ************\n") == str(1):
     for action in wallet:
-        summary = "Nom : %s, Prix/Cout : %s, Bénéfices attendus : %s" % (action[0], float(action[1]), float(action[2]))
-        print(summary)
+        action_summary = "Nom : %s, Prix/Cout : %s, Bénéfices attendus /2ans : %s" % (action[0], float(action[1]),
+                                                                                      float(action[2]))
+        print(action_summary)
