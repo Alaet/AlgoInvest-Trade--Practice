c7ba1c83239d29c3187e2c98baae963fd5ff4055
Alaet <72195619+Alaet@users.noreply.github.com>
2022-01-29 12:02:50 +0100

    Update optimized.py
    added wallet completion until capital is reach
    to do : update best_wallet in loop based on benefit const

--- optimized.py
+++ optimized.py
@@ -18,9 +18,29 @@ def read_files(files_names):
 actions = read_files(["data/dataset1_Python.csv", "data/dataset2_Python.csv"])
 
 
+def calcul_wallet_benefit(actions):
+    benefit_best_wallet = 0
+    for act in actions:
+        benefit_best_wallet += (act[2] / 100) * act[1]
+    return benefit_best_wallet
+
+
 def get_best_wallet(actions, capital):
     i = 0
     actions.sort(key=lambda x:x[2], reverse=True)
     best_wallet = []
-    
+
+    while i <= capital:
+        allocated_capital = i
+        current_wallet = []
+
+        current_wallet_benefit = 0
+        for current_action in actions:
+            if current_action[1] <= allocated_capital:
+                current_wallet.append(current_action)
+                allocated_capital -= current_action[1]
+                current_wallet_benefit += (current_action[2]/100)*current_action[1]
+
+        benefit = calcul_wallet_benefit(best_wallet)
+
 print(actions)
