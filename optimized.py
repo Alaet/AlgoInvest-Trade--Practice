dd6175b391c8411a2084e759a4b385a939834806
Alaet <72195619+Alaet@users.noreply.github.com>
2022-02-01 13:22:31 +0100

    update opti
    loop for n item to check even more combination
    todo : check with adding previous actions to futur investigation (through copied array)

--- optimized.py
+++ optimized.py
@@ -19,6 +19,19 @@ def read_files(files_names):
     return actions_list
 
 
+def read_file(file_name):
+    actions_list = []
+    file = open(file_name)
+    csvreader_file = csv.reader(file)
+    for action_row in csvreader_file:
+        if action_row[1] != "price":
+            action_row[1] = float(action_row[1])
+            action_row[2] = float(action_row[2])
+            if action_row[1] > 0 and action_row[2] > 0:
+                actions_list.append(action_row)
+    return actions_list
+
+
 def get_wallet_benefit(actions_to_evaluate):
     benefit_best_wallet = 0
     for act in actions_to_evaluate:
@@ -38,24 +51,25 @@ def get_best_wallet(actions, capital):
     best_wallet = Wallet([])
     allocated_capital = capital
     current_wallet = Wallet([])
-    for x, current_action in enumerate(actions):
-        if current_action[1] <= allocated_capital:
-            current_wallet.actions.append(current_action)
-            allocated_capital -= current_action[1]
-            current_wallet.benefit += (current_action[2] / 100) * current_action[1]
-            current_wallet.cost = get_wallet_invest_cost(current_wallet.actions)
-        if current_wallet.benefit > best_wallet.benefit:
-            best_wallet = current_wallet
+    for i in range(0, len(actions)):
+        for current_action in actions:
+            if current_action[1] <= allocated_capital:
+                current_wallet.actions.append(current_action)
+                allocated_capital -= current_action[1]
+                current_wallet.benefit += (current_action[2] / 100) * current_action[1]
+                current_wallet.cost = get_wallet_invest_cost(current_wallet.actions)
+            if current_wallet.benefit > best_wallet.benefit:
+                best_wallet = current_wallet
     return best_wallet
 
 
 list_actions = small_list_actions
 all_csv_actions = read_files(["data/dataset1_Python.csv", "data/dataset2_Python.csv"])
-csv_actions1 = read_files(["data/dataset1_Python.csv"])
-csv_actions2 = read_files(["data/dataset2_Python.csv"])
+csv_actions1 = read_file("data/dataset1_Python.csv")
+csv_actions2 = read_file("data/dataset2_Python.csv")
 timer = time.time()
-wallet = get_best_wallet(all_csv_actions, 500)
+wallet = get_best_wallet(csv_actions1, 500)
 print("************     Temps d'éxécution du calcul:"
       "     ************\n----------->     %s seconde(s)" % (time.time()-timer))
 wallet.cost = get_wallet_invest_cost(wallet.actions)
-display_top_wallet(wallet, all_csv_actions)
+display_top_wallet(wallet, csv_actions1)
