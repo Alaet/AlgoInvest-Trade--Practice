e184ab7b92ca39c419a5ef8a4cf7f7c5c52302dc
Alaet <72195619+Alaet@users.noreply.github.com>
2022-02-03 16:15:47 +0100

    Rename optimized.py
    for correct greedy approach name

--- optimized_greedy.py
+++ optimized_greedy.py
@@ -4,6 +4,8 @@ from wallet.view import display_top_wallet
 from wallet.model import Wallet
 import time
 
+timer = time.time()
+
 
 def read_files(files_names):
     actions_list = []
@@ -32,20 +34,6 @@ def read_file(file_name):
     return actions_list
 
 
-def get_wallet_benefit(actions_to_evaluate):
-    benefit_best_wallet = 0
-    for act in actions_to_evaluate:
-        benefit_best_wallet += (act[2] / 100) * act[1]
-    return benefit_best_wallet
-
-
-def get_wallet_invest_cost(actions_to_evaluate):
-    wallet_investment = 0
-    for act in actions_to_evaluate:
-        wallet_investment += act[1]
-    return wallet_investment
-
-
 def get_best_wallet(actions, capital):
     actions.sort(key=lambda x: x[2], reverse=True)
     best_wallet = Wallet([])
@@ -56,20 +44,17 @@ def get_best_wallet(actions, capital):
             current_wallet.actions.append(current_action)
             allocated_capital -= current_action[1]
             current_wallet.benefit += (current_action[2] / 100) * current_action[1]
-            current_wallet.cost = get_wallet_invest_cost(current_wallet.actions)
+            current_wallet.cost += current_action[1]
     if current_wallet.benefit > best_wallet.benefit:
         best_wallet = current_wallet
-    return best_wallet
+    print("************     Temps d'éxécution du calcul:"
+          "     ************\n----------->     %s " % (time.time() - timer))
+    best_wallet.ratio = round(best_wallet.benefit/best_wallet.cost*100, 2)
+    display_top_wallet(best_wallet, all_csv_actions)
 
 
 list_actions = small_list_actions
 all_csv_actions = read_files(["data/dataset1_Python.csv", "data/dataset2_Python.csv"])
 csv_actions1 = read_file("data/dataset1_Python.csv")
 csv_actions2 = read_file("data/dataset2_Python.csv")
-timer = time.time()
-wallet = get_best_wallet(all_csv_actions, 500)
-
-print("************     Temps d'éxécution du calcul:"
-      "     ************\n----------->     %s seconde(s)" % (time.time()-timer))
-display_top_wallet(wallet, all_csv_actions)
-
+get_best_wallet(all_csv_actions, 500)
